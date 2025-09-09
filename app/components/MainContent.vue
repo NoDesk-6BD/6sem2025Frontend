bcfd333070283ae323e23465da94fd07f7992de4
DankoCaboski <caboskidanko@gmail.com>
2025-09-09 19:54:27 -0300

    Feat: mainContent

---
+++ app/components/MainContent.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <criticalProjects />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>Bem vindo a pagina Inicial</h1>
-        <AppAlert>Testando o componente de alerta</AppAlert>
+    <div class="container">
+      <div class="sidebar">
+        <sideBar />
+      </div>
+      <div class="main-content">
+        <MainContent />
+      </div>
     </div>
 </template>
