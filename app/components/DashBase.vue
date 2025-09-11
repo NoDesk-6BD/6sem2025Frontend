9da1c2e19a49f57c2b56b8e78269a4e8208d2625
Wallace Caetano <wallacesantoscs@gmail.com>
2025-09-11 20:10:21 -0300

    refactor (ND-41): DashBase use UCard

--- app/components/DashBase.vue
+++ app/components/DashBase.vue
@@ -1,21 +1,23 @@
-<template>
-  <v-card class="dashboard-card" elevation="2">
-    <v-card-title>{{ props.dashName }}</v-card-title>
-    <component :is="props.component" />
-  </v-card>
-</template>
-
-<script lang="ts" setup>
-import { defineProps } from 'vue';
-
-import type { Component } from 'vue';
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Component } from 'vue'
 
 const props = defineProps<{
-  component: string | Component,
   dashName: string
-}>();
+  component: string | Component
+}>()
 </script>
 
-<style>
+<template>
+  <UCard class="bg-white shadow-md rounded-lg p-3">
+    <template #header>
+      <div class="text-gray-500 font-medium text-sm">
+        {{ props.dashName }}
+      </div>
+    </template>
 
-</style>
+    <div class="mt-2">
+      <component :is="props.component" class="w-full h-64" />
+    </div>
+  </UCard>
+</template>
