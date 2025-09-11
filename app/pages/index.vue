41622bd070f70770ce1619dac1fd531872f86dd9
Wallace Caetano <wallacesantoscs@gmail.com>
2025-09-11 20:14:50 -0300

    refactor (ND-41): index  page use MetricsCard

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
-  <div  class="grid grid-cols-9 gap-4">
-    <sideBar user-name="Usuário"/>
-    <MainContent class="col-span-8"/>
+  <div class="main-content">
+    <div class="grid grid-cols-4 gap-2 mb-5">
+      <MetricsCard titulo-metrica="Metrica 1" valor-metrica="1"/>
+      <MetricsCard titulo-metrica="Metrica 2" valor-metrica="2"/>
+      <MetricsCard titulo-metrica="Metrica 3" valor-metrica="3"/>
+      <MetricsCard titulo-metrica="Metrica 4" valor-metrica="4"/>
+    </div>
+    <div class="dash-boards">
+      <dash-base component="Dashboard1" dash-name="Dashboard 1" />
+      <dash-base component="Dashboard2" dash-name="Dashboard 2" />
+      <dash-base component="Dashboard3" dash-name="Dashboard 3" />
+      <dash-base component="Dashboard4" dash-name="Dashboard 4" />
+    </div>
   </div>
 </template>
