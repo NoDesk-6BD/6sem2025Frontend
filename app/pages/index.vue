9ffc4d7ddbd26f6d38fa6cad760ccd12ed030d58
DankoCaboski <caboskidanko@gmail.com>
2025-09-16 20:02:38 -0300

    fix (ND-27): add header for index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,35 +4,41 @@ const chartTitleClass = "text-gray-500 font-medium text-xl";
 
 <template>
   <div class="main-content">
-    <div class="grid grid-cols-4 gap-5 mb-7">
-      <MetricsCard
-        titulo-metrica="Metrica 1"
-        :valor-metrica="15"
-        :bottom-limit="3"
-        :top-limit="10"
-        :relation="true"
-      />
-      <MetricsCard
-        titulo-metrica="Metrica 2"
-        :valor-metrica="31"
-        :bottom-limit="3"
-        :top-limit="10"
-        :relation="false"
-      />
-      <MetricsCard
-        titulo-metrica="Metrica 3"
-        :valor-metrica="77"
-        :bottom-limit="3"
-        :top-limit="10"
-        :relation="true"
-      />
-      <MetricsCard
-        titulo-metrica="Metrica 4"
-        :valor-metrica="42"
-        :bottom-limit="40"
-        :top-limit="50"
-        :relation="true"
-      />
+    <div>
+      <div class="grid grid-cols-2 mb-7">
+        <h1 class="text-2xl font-bold mb-4 self-center">KPI Cards</h1>
+        <TimeFilter />
+      </div>
+      <div class="grid grid-cols-4 gap-5 mb-7">
+        <MetricsCard
+          titulo-metrica="Metrica 1"
+          :valor-metrica="15"
+          :bottom-limit="3"
+          :top-limit="10"
+          :relation="true"
+        />
+        <MetricsCard
+          titulo-metrica="Metrica 2"
+          :valor-metrica="31"
+          :bottom-limit="3"
+          :top-limit="10"
+          :relation="false"
+        />
+        <MetricsCard
+          titulo-metrica="Metrica 3"
+          :valor-metrica="77"
+          :bottom-limit="3"
+          :top-limit="10"
+          :relation="true"
+        />
+        <MetricsCard
+          titulo-metrica="Metrica 4"
+          :valor-metrica="42"
+          :bottom-limit="40"
+          :top-limit="50"
+          :relation="true"
+        />
+      </div>
     </div>
 
     <div class="grid grid-cols-2 gap-5">
