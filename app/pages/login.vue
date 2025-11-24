9a4c4218379ba52159f80c61c613675f134a395c
DankoCaboski <caboskidanko@gmail.com>
2025-11-24 00:34:03 -0300

    feat (ND-51): assert user credentials at login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -6,12 +6,12 @@
 
       <form class="space-y-4" @submit.prevent="handleLogin">
         <div>
-          <label class="block text-sm font-medium mb-1">Usuário</label>
+          <label class="block text-sm font-medium mb-1">Email</label>
           <input
-            v-model="username"
+            v-model="email"
             type="text"
             class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-            placeholder="Digite seu usuário"
+            placeholder="Digite seu email"
           />
         </div>
 
@@ -37,15 +37,35 @@
 </template>
 
 <script setup lang="ts">
+import { useRuntimeConfig } from "#app";
+const config = useRuntimeConfig();
+
 definePageMeta({
   layout: "auth",
 });
 
-const username = ref("");
+const email = ref("");
 const password = ref("");
 
 function handleLogin() {
-  console.log("Realizando login:", username.value, password.value);
-  navigateTo("/dashboard");
+  console.log("Realizando login:", email.value, password.value);
+  Login(email.value, password.value);
+}
+
+async function Login(email?: string, password?: string) {
+  try {
+    const response = await $fetch(`${config.public.apiBase}/auth/login`, {
+      method: "POST",
+      body: { email: email, password: password },
+    });
+    if (response) {
+      localStorage.setItem("auth_token", response.access_token);
+      navigateTo("/dashboard");
+    } else {
+      console.log("Falha no login: Credenciais inválidas");
+    }
+  } catch (err) {
+    console.error("Erro ao realizar login:", err);
+  }
 }
 </script>
